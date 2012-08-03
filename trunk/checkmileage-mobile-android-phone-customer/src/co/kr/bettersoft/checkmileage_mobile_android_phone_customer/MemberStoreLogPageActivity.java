01e8e8816cd5eff8b5484393991c7f8fda787f43
evacdi86@gmail.com <evacdi86@gmail.com@78bceef7-139b-007a-17a4-286f572f907a>
2012-08-03 06:03:07 +0000

    add files..

---
+++ trunk/checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/MemberStoreLogPageActivity.java
@@ -0,0 +1,16 @@
+package co.kr.bettersoft.checkmileage_mobile_android_phone_customer;
+// 가맹점 상세 - 이용 내역
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MemberStoreLogPageActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
---
+++ trunk/checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/Settings_MyInfoPageActivity.java
@@ -0,0 +1,16 @@
+package co.kr.bettersoft.checkmileage_mobile_android_phone_customer;
+// 개인정보 변경
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings_MyInfoPageActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
