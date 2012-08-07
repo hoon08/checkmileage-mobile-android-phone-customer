74643a6456ea49105919504c4e5ee212c64c7302
evacdi86@gmail.com <evacdi86@gmail.com@78bceef7-139b-007a-17a4-286f572f907a>
2012-08-07 06:47:24 +0000

    어플이 실행될때에 QR코드를QRcode.txt 파일로 부터 읽음.  파일이 없다면 최초 실행이므로 QRcode 파일을 생성함. (내부에 QR코드값없음)
    파일이 없거나 파일 안에 QR 코드 값이 없는 경우 
      인증(미구현)을 거쳐 QR생성 화면으로 이동. (현재는 바로 QR생성으로 이동)
     (*인증을 거쳐 이전 QR코드 값을 얻게 될 경우는 내 QR코드 보기로 이동)
    
    QR코드 값이 있을 경우 그 코드 값을 내 QR코드페이지에 저장하고
      그 값으로 구글 웹 통신하여 QR코드 이미지 생성하여 화면에 보여줌.
    
    
    QR 등록 화면에서 신규QR생성시 하드코딩된 문자열을 QR코드로 하여
     QR코드 파일에 저장하고, 해당 문자열을 내QR코드보기 화면에 저장하여
      내 QR코드보기 화면에서 이 값으로 구글 웹 통신하여 QR코드 이미지 받아와서 보여줌

--- trunk/checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/CreateQRPageActivity.java
+++ trunk/checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/CreateQRPageActivity.java
@@ -15,6 +15,8 @@ import android.util.Log;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -29,72 +31,63 @@ import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-
+/* 
+ * QR 을 생성하고 바로 다음단계인 나의 QR 코드 보기액티비티로 넘어간다.
+ * 사용자에게 이 액티비티는 보여지지 않고 바로 나의 QR 코드보기 화면이 나타나게 된1다.
+ */
 public class CreateQRPageActivity extends Activity {
-
+	static int qrResult = 0;
+	String qrcode = "createdNewQRCodeOne";
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
-	    /* 
-		  * QR 을 생성하고 바로 다음단계인 나의 QR 코드 보기액티비티로 넘어간다.
-		  * 사용자에게 이 액티비티는 보여지지 않고 바로 나의 QR 코드보기 화면이 나타나게 된1다.
-		  */
-		    
+	    /*
+	     *  서버와 통신하여 QR 생성.
+	     */
 	    // QR 코드 자체 생성하는 부분..
 	    // ... QR 코드를 생성하고, 서버에 등록한다.
+	    // 현재 위의 하드코딩 텍스트 사용함.
 	    
-		
-//	    try {
-//			CreateQR();
-//		} catch (WriterException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		    
-	    // 나의 QR 코드 보기로 이동.
-	    Log.i("CreateQRPageActivity", "QR registered Success");
-		 Intent intent2 = new Intent(CreateQRPageActivity.this, Main_TabsActivity.class);
-	        startActivity(intent2);
-	        finish();
+	    /*
+	     * QR 저장소 파일에 저장.
+	     */
+	    Log.i("CreateQRPageActivity", "save qrcode to file : "+qrcode);
+	    CommonUtils.writeQRstr = qrcode;
+	    saveQR();
+	    
+	    /*
+	     * MyQR페이지에 생성된 QR로 QR이미지 받아서 보여줌.
+	     */
+	    Log.i("CreateQRPageActivity", "load qrcode to img : "+qrcode);
+	    MyQRPageActivity.qrCode = qrcode;
+
+	    new Thread(
+	    		new Runnable(){
+	    			public void run(){
+	    				try{
+	    					Thread.sleep(1000);
+	    					Log.i("CreateQRPageActivity", "qrResult::"+qrResult);		// 읽기 결과 받음.
+
+	    					// 나의 QR 코드 보기로 이동.
+	    					Log.i("CreateQRPageActivity", "QR registered Success");
+	    					Intent intent2 = new Intent(CreateQRPageActivity.this, Main_TabsActivity.class);
+	    					startActivity(intent2);
+	    					finish();		// 다른 액티비티를 호출하고 자신은 종료.
+	    				}catch(InterruptedException ie){
+	    					ie.printStackTrace();
+	    				}
+	    			}
+	    		}
+	    ).start();
 	}
-
 	
 	
-	
-	
-	
-	@SuppressWarnings("null")
-	public static void CreateQR() throws WriterException, IOException {
-        QRCodeWriter qrCodeWriter = new QRCodeWriter();
-        String text = "test1234";
-        text = new String(text.getBytes("UTF-8"), "ISO-8859-1");
-        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE,
-                100, 100);
-//        
-//        Log.i("CreateQRPageActivity", "lv2");
-//        String st =  bitMatrix.toString();
-//        Log.i("CreateQRPageActivity", "lv3");
-//        byte[] data  = st.getBytes();
-//        Log.i("CreateQRPageActivity", "lv4");
-//        ByteArrayInputStream in = new ByteArrayInputStream(data);
-//        Log.i("CreateQRPageActivity", "lv5");
-//        Bitmap bm=null; 
-//        BitmapDrawable bmd = new BitmapDrawable(in); 
-//        //	        	in.read(data);		// null pointer exception
-//        Log.i("CreateQRPageActivity", "lv6");
-////        Bitmap bitmap = BitmapFactory.decodeStream(in);  
-//        
-//        bm = bmd.getBitmap(); 
-////        Log.i("CreateQRPageActivity", "6-1"+bitmap.getHeight());
-//
-//        Log.i("CreateQRPageActivity", "lv7");
-
-////        MatrixToImageWriter.writeToFile(bitMatrix, "png", new File("qrcode.png"));	// cant find class exception
+	// QR 코드 저장소에 QR 코드를 저장한다. 
+    public void saveQR(){		
+    	CommonUtils.callCode = 22;		// 쓰기 모드
+    	Intent saveQRintent = new Intent(CreateQRPageActivity.this, CommonUtils.class);			// 호출
+    	startActivity(saveQRintent);
     }
 
-	
 }
--- trunk/checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/MainActivity.java
+++ trunk/checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/MainActivity.java
@@ -2,7 +2,9 @@ package co.kr.bettersoft.checkmileage_mobile_android_phone_customer;
 // 인트로
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 import android.os.Bundle;
@@ -23,7 +25,6 @@ import android.widget.Toast;
  *  
  */
 
-
 public class MainActivity extends Activity {
 	String TAG = "MainActivity";
 	Handler handler = new Handler(){
@@ -36,14 +37,20 @@ public class MainActivity extends Activity {
     		}
     	}
     };
-	
+    static String myQR = "";
+    static int qrResult = 0;
     
+    static CommonUtils cu = new CommonUtils();
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	Log.i("MainActivity", "Success Starting MainActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro);
         
+        readQR();			// 일단 저장된 QR 값이 있는지부터 확인 한다.. 
+//        saveQR();			// QR 코드 저장소에 임시 값 저장. 테스트용. 
+//        initialQR();		// QR 코드 저장소에 값 초기화. 테스트용.
+//        Log.i("MainActivity", "qrResult::"+qrResult);		// 아직 결과 받기 전이기 때문에 여기서 확인 불가.. 아래 thread 내부에서 확인 가능.
         
         new Thread(
         		new Runnable(){
@@ -52,26 +59,32 @@ public class MainActivity extends Activity {
         					Thread.sleep(1000);
         					// 잠금기능 사용시 비번 입력 페이지로 이동..
         					     // 은 아직 미구현.
-        					
+        					Log.i("MainActivity", "qrResult::"+qrResult);		// 읽기 결과 받음.
+        					while(qrResult!=1){		// 최초 실행시 파일 읽기 실패함(파일없음에러. 에러코드:-3) --> 새로 생성한다.
+        						Log.i("MainActivity", "there is no saved file detected.. generate new one.");	
+        						initialQR();
+        						Thread.sleep(1000);
+        					}
         					// QR 코드가 있다면 QR 화면으로 이동하고, QR 코드가 없다면 QR 등록 화면으로 이동한다.
-        					if(false){
-        						// ... // QR코드가 있는지 확인. properties 파일 이용.
-        						Log.i("MainActivity", "Go Main Pages");
+        					if(myQR.length()>0){ // QR코드가 있는지 확인. 있으면 바로 내 QR 페이지로 이동.
+        						Log.i("MainActivity", "QR code checked success, Go Main Pages::"+myQR);
         						Intent intent = new Intent(MainActivity.this, Main_TabsActivity.class);
-        						
         						startActivity(intent);
-        						
-        						
-        						
-        						
         						finish();		// 다른 액티비티를 호출하고 자신은 종료. 
-        					}else {
-        						Log.i("MainActivity", "Go get QR");
-//        						Intent intent = new Intent(MainActivity.this, No_QR_PageActivity.class);
-        						Intent intent = new Intent(MainActivity.this, CommonUtils.class);
+        					}else {				// QR 코드가 없으면 설치후 최초 실행하는 사람. 
+        						/*
+        						 *  기존에 인증 받은 사용자인지 확인이 필요하다.
+        						 *  QR 저장 파일에 QR 값이 없을 시에는 어플 설치 후 최초 실행이므로 인증을 받아야 한다..
+        						 *  인증 1단계인 [휴대폰 번호 인증]으로  서버와 통신을 하여 이전 등록된 사용자인지 확인을 한다. (이전 등록된 사용자라면 이전 등록한 QR 코드를 받아서 그대로 사용) 
+        						 *  서버에도 QR 값이 없을 경우에는 2차 인증(인증번호 인증) 후에 QR 생성 선택 창으로 이동한다.
+        						 *  1차 인증을 통해 서버에서 QR 값을 받아온 경우 인증 2단계인 [인증번호 확인] 절차를 생략하고 내 QR보기 화면으로 이동한다. 
+        						 */
+        				        
+        						//QR 생성 선택 창으로 이동.
+        						Log.i("MainActivity", "There is no saved QR code.. Go get QR");
+        						Intent intent = new Intent(MainActivity.this, No_QR_PageActivity.class);
         						startActivity(intent);
         						finish();		// 다른 액티비티를 호출하고 자신은 종료.
-        						// ...
         					}
         				}catch(InterruptedException ie){
         					ie.printStackTrace();
@@ -79,12 +92,47 @@ public class MainActivity extends Activity {
         			}
         		}
         ).start();
-
     }
-
+    
+    // QR 코드 저장소에서 QR 코드를 읽어온다.
+    public void readQR(){
+    	CommonUtils.callCode = 1;			// 읽기 모드.
+    	Intent getQRintent = new Intent(MainActivity.this, CommonUtils.class);		// 호출
+    	startActivity(getQRintent);
+    }
+    
+    // QR 코드 저장소에 QR 코드를 저장한다. (테스트용)
+    public void saveQR(){		
+    	CommonUtils.callCode = 2;		// 쓰기 모드
+    	Intent saveQRintent = new Intent(MainActivity.this, CommonUtils.class);			// 호출
+    	startActivity(saveQRintent);
+    }
+    
+    // QR 코드 저장소에 있는 QR 코드 정보를 초기화한다. (테스트용)
+    public void initialQR(){		
+    	CommonUtils.callCode = 3;		// 초기화 모드
+    	Intent initQRintent = new Intent(MainActivity.this, CommonUtils.class);		// 호출
+    	startActivity(initQRintent);
+    }
+    
+    
+    // 하드웨어 메뉴 버튼 눌렀을 경우... 사용 하지 않을 예정.
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    
+    // 인텐트를 결과를 받는 용도로 호출하였을 경우 (어떤 호출인지 구분은 requestCode 를 사용) 그 결과를 받아서 처리하는 부분.
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	super.onActivityResult(requestCode, resultCode, data);
+    	Log.e(TAG, "onActivityResult");
+    	if(requestCode == 201) {							
+    		if(resultCode == RESULT_OK) {			
+    			// ...
+    		}
+    	}
     }
 }
