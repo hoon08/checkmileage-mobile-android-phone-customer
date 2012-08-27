a7aea971379960287d76447942902b0aa7b58ffe
evacdi86 <evacdi86@gmail.com>
2012-08-27 01:21:43 +0000

    0827 업뎃중 누락된 것 추가.

---
+++ checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/myWebView.java
@@ -0,0 +1,76 @@
+package co.kr.bettersoft.checkmileage_mobile_android_phone_customer;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class myWebView extends Activity {
+	WebView mWeb;
+	String loadingURL = "";
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.my_web_view);
+	    
+	    Intent rIntent = getIntent();
+	    loadingURL = rIntent.getStringExtra("loadingURL");			// URL 정보를 받음.
+	    
+	    mWeb = (WebView)findViewById(R.id.web);
+	    
+	 // 이미지 확대/축소/스크롤 금지. -> 허용으로 수정.  --> 금지
+//		mWeb.getSettings().setUseWideViewPort(true);		
+	    
+		mWeb.setWebChromeClient(new MyWebChromeClient());
+		WebSettings webSet = mWeb.getSettings();
+		
+		// JavaScript 허용.
+		webSet.setJavaScriptEnabled(true);
+
+		// 확대/축소 금지. -> 허용으로 수정.  --> 금지
+//		webSet.setSupportZoom(true);
+//		webSet.setBuiltInZoomControls(true);
+		
+		if(loadingURL.length()>0){
+			mWeb.loadUrl(loadingURL);		// url
+		}else{
+			Toast.makeText(myWebView.this, "요청하신 주소를 찾을 수 없습니다.", Toast.LENGTH_SHORT).show();
+		}
+		
+	}
+	/**
+	 * WebChromeClient 를 상속하는 클래스이다.
+	 * alert 이나 윈도우 닫기 등의 web 브라우저 이벤트를 구하기 위한 클래스이다.
+	 * 
+	 * @author johnkim
+	 *
+	 */
+	private class MyWebChromeClient extends WebChromeClient {
+
+		//Javascript alert 호출 시 실행
+		@Override
+		public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+			final JsResult finalRes = result;
+
+			//AlertDialog 생성
+			new AlertDialog.Builder(view.getContext())
+			.setMessage(message)
+			.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+				public void onClick(DialogInterface dialog, int which) {
+					finalRes.confirm(); 
+				}
+			})
+			.setCancelable(false)
+			.create()
+			.show();
+			return true;
+		}
+	}
+}
