6cd11aac7e0d98d042edd0765555a9a452ed5ed9
evacdi86@gmail.com <evacdi86@gmail.com@78bceef7-139b-007a-17a4-286f572f907a>
2012-10-19 05:51:41 +0000

    10/19 (2)  일본어 번역 xml 파일, 푸시 액티비티 페이지

---
+++ trunk/checkmileage-mobile-android-phone-customer/src/co/kr/bettersoft/checkmileage_mobile_android_phone_customer/PushList.java
@@ -0,0 +1,20 @@
+package co.kr.bettersoft.checkmileage_mobile_android_phone_customer;
+/*
+ * 가맹점에서 보내온 이벤트 목록보기. 특정 버튼 터치하여 상세 화면으로 이동 가능.
+ * 
+ * 
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PushList extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.push_list);
+	    // TODO Auto-generated method stub
+	}
+
+}
